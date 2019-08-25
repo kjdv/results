dc14ae6a32c05c6811c31f946969e8a4a05e3e03
Klaas de Vries <klaasjacobdevries@gmail.com>
2019-08-25 21:07:55 +0100

    map

--- src/option.hh
+++ src/option.hh
@@ -9,57 +9,53 @@ namespace results {
 // todo: move to dedicated file/namespace
 [[noreturn]] void panic(std::string_view msg);
 
-// c++17 magic...
-template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
-template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
-
 template <typename T>
 class option {
 public:
-    option()
-    {}
-    option(T value)
-        : d_value(std::move(value))
-    {}
+    using value_type = T;
 
-    bool is_none() const {
-        return !d_value.has_value();
-    }
+    option();
+    option(T value);
 
-    bool is_some() const {
-        return d_value.has_value();
-    }
+    bool is_none() const;
 
-    const T & unwrap() const {
-        return expect("unwrapping none");
-    }
+    bool is_some() const;
 
-    const T & expect(std::string_view msg) const {
-        if (!is_some()) {
-            panic(msg);
-        }
-        return *d_value;
-    }
+    const T & unwrap() const;
+
+    const T & expect(std::string_view msg) const;
+
+    const option<T> &and_(const option<T> &other) const;
 
     template <typename F>
-    auto and_then(F && f) const -> option<decltype(f(unwrap()))> {
-        using U = decltype(f(unwrap()));
-        if (is_none()) {
-            return option<U>();
-        }
-        return option<U>(f(*d_value));
-    }
+    auto and_then(F && f) const -> decltype(f(unwrap()));
 
     template <typename F1, typename F2>
-    auto match(F1 && on_some, F2 && on_none) -> decltype(on_some(unwrap())) {
-        static_assert(std::is_constructible<decltype(on_none()), decltype(on_some(unwrap()))>::value,
-                      "return value of on_none() must be equal or convertible to the return value of on_some()");
+    auto match(F1 && on_some, F2 && on_none) const -> decltype(on_some(unwrap()));
 
-        if (is_some()) {
-            return on_some(*d_value);
-        }
-        return on_none();
-    }
+    template <typename P>
+    option<T> filter(P && predicate) const;
+
+    const option<T> &or_(const option<T> &other) const;
+
+    template <typename F>
+    option<T> or_else(F && f) const;
+
+    T &get_or_insert(T value);
+
+    template <typename F>
+    T &get_or_insert_with(F && f);
+
+    template <typename F>
+    auto map(F && f) const -> option<std::decay_t<decltype(f(unwrap()))>>;
+
+    template <typename F, typename U>
+    auto map_or(F && f, const U &def) const -> decltype(f(unwrap()));
+
+    template <typename F1, typename F2>
+    auto map_or_else(F1 && f, const F2 &def) const -> decltype(f(unwrap()));
+
+
 private:
     std::optional<T> d_value;
 };
@@ -73,6 +69,139 @@ template <typename T, typename... Args>
 option<std::decay_t<T>> make_some(Args&&... args) {
     return option<std::decay_t<T>>(T(std::forward<Args>(args)...));
 }
+
+template <typename T>
+option<T> flatten(const option<option<T>> &o) {
+    return o.is_some() ? o.unwrap() : make_none<T>();
+}
+
+template<typename T>
+option<T>::option()
+{}
+
+template<typename T>
+option<T>::option(T value)
+    : d_value(std::move(value))
+{}
+
+template<typename T>
+bool option<T>::is_none() const {
+    return !d_value.has_value();
+}
+
+template<typename T>
+bool option<T>::is_some() const {
+    return d_value.has_value();
+}
+
+template<typename T>
+const T &option<T>::unwrap() const {
+    return expect("unwrapping none");
+}
+
+template<typename T>
+const T &option<T>::expect(std::string_view msg) const {
+    if (!is_some()) {
+        panic(msg);
+    }
+    return *d_value;
+}
+
+template<typename T>
+const option<T> &option<T>::and_(const option<T> &other) const {
+    return is_none() ? *this : other;
+}
+
+template<typename T>
+const option<T> &option<T>::or_(const option<T> &other) const {
+    return is_some() ? *this : other;
+}
+
+template<typename T>
+T &option<T>::get_or_insert(T value) {
+    if (is_none()) {
+        d_value = std::move(value);
+    }
+    return *d_value;
+}
+
+
+template<typename T>
+template<typename F>
+T &option<T>::get_or_insert_with(F &&f)
+{
+    static_assert(std::is_convertible<decltype(f()), T>::value, "the return type of f() must be convertible to T");
+    if (is_none()) {
+        d_value = f();
+    }
+    return *d_value;
+}
+
+template<typename T>
+template <typename F>
+option<T> option<T>::or_else(F && f) const {
+    static_assert(std::is_convertible<option<T>, decltype (f())>::value, "the return type of f() must be convertible to T");
+    return is_some() ? *this : f();
+}
+
+template<typename T>
+template<typename F>
+auto option<T>::and_then(F && f) const -> decltype(f(unwrap())) {
+    using U = decltype(f(unwrap()));
+    if (is_none()) {
+        return U();
+    }
+    return f(*d_value);
+}
+
+template <typename T>
+template <typename F1, typename F2>
+auto option<T>::match(F1 && on_some, F2 && on_none) const -> decltype(on_some(unwrap())) {
+    static_assert(std::is_constructible<decltype(on_none()), decltype(on_some(unwrap()))>::value,
+                  "return value of on_none() must be equal or convertible to the return value of on_some()");
+
+    if (is_some()) {
+        return on_some(*d_value);
+    }
+    return on_none();
+}
+
+template <typename T>
+template <typename P>
+option<T> option<T>::filter(P && predicate) const {
+    if (is_none() || !predicate(*d_value)) {
+        return make_none<T>();
+    }
+    return make_some<T>(*d_value);
+}
+
+template <typename T>
+template <typename F>
+auto option<T>::map(F && f) const -> option<std::decay_t<decltype(f(unwrap()))>> {
+    using U = std::decay_t<decltype(f(unwrap()))>;
+    if (is_none()) {
+        return option<U>();
+    }
+    return option<U>(f(*d_value));
+}
+
+template <typename T>
+template <typename F, typename U>
+auto option<T>::map_or(F && f, const U &def) const -> decltype(f(unwrap())) {
+   if (is_none()) {
+      return def;
+   }
+   return f(*d_value);
+}
+
+
+template <typename T>
+template <typename F1, typename F2>
+auto option<T>::map_or_else(F1 && f, const F2 &def) const -> decltype(f(unwrap())) {
+    return is_some() ? f(*d_value) : def();
+
+}
+
 
 }
 
--- test/option.cc
+++ test/option.cc
@@ -33,7 +33,7 @@ TEST(option, expect) {
 }
 
 TEST(option, and_then) {
-    auto to_string = [](int i) { return std::to_string(i); };
+    auto to_string = [](int i) { return make_some<std::string>(std::to_string(i)); };
     EXPECT_EQ("42", make_some<int>(42).and_then(to_string).unwrap());
 
     EXPECT_TRUE(make_none<int>().and_then(to_string).is_none());
@@ -43,6 +43,74 @@ TEST(option, match) {
     EXPECT_EQ(2, make_some<int>(2).match([](int i) { return i; }, []{ return -1; }));
     EXPECT_EQ(-1, make_none<int>().match([](int i) { return i; }, []{ return -1; }));
 }
+
+TEST(option, filter) {
+    auto pred = [](int i) { return i == 3; };
+
+    EXPECT_EQ(3, make_some<int>(3).filter(pred).unwrap());
+    EXPECT_TRUE(make_some<int>(4).filter(pred).is_none());
+    EXPECT_TRUE(make_none<int>().filter(pred).is_none());
+}
+
+TEST(option, or_else) {
+    EXPECT_EQ(2, make_some<int>(2).or_(make_some<int>(3)).unwrap());
+    EXPECT_EQ(2, make_none<int>().or_(make_some<int>(2)).unwrap());
+
+    EXPECT_EQ(2, make_some<int>(2).or_else([]{ return make_some<int>(3); }).unwrap());
+    EXPECT_EQ(2, make_none<int>().or_else([]{ return make_some<int>(2); }).unwrap());
+}
+
+TEST(option, and_) {
+    auto some = make_some<int>(0);
+    auto none = make_none<int>();
+
+    EXPECT_TRUE(none.and_(some).is_none());
+    EXPECT_TRUE(some.and_(none).is_none());
+    EXPECT_TRUE(some.and_(make_some<int>(1)).is_some());
+}
+
+TEST(option, flatten) {
+    auto some = make_some<option<int>>(make_some<int>(3));
+    auto none = make_none<option<int>>();
+
+    EXPECT_EQ(3, flatten(some).unwrap());
+    EXPECT_TRUE(flatten(none).is_none());
+}
+
+TEST(option, get_or_insert) {
+    auto some = make_some<int>(0);
+    auto none = make_none<int>();
+
+    EXPECT_EQ(0, some.get_or_insert(2));
+    EXPECT_EQ(2, none.get_or_insert(2));
+
+    none = make_none<int>();
+
+    auto f = []{ return 2; };
+    EXPECT_EQ(0, some.get_or_insert_with(f));
+    EXPECT_EQ(2, none.get_or_insert_with(f));
+}
+
+TEST(option, map) {
+    auto to_string = [](int i) { return std::to_string(i); };
+    EXPECT_EQ("42", make_some<int>(42).map(to_string).unwrap());
+
+    EXPECT_TRUE(make_none<int>().map(to_string).is_none());
+}
+
+TEST(option, map_or) {
+    auto f = [](int i) { return i * 2; };
+    EXPECT_EQ(2, make_none<int>().map_or(f, 2));
+    EXPECT_EQ(6, make_some<int>(3).map_or(f, 2));
+}
+
+TEST(option, map_or_else) {
+    auto f = [](int i) { return i * 2 ;};
+    auto g = []{ return 42; };
+    EXPECT_EQ(42, make_none<int>().map_or_else(f, g));
+    EXPECT_EQ(6, make_some<int>(3).map_or_else(f, g));
+}
+
 
 }
 }
